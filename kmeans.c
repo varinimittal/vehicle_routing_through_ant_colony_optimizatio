420ab1f1ed941cf4e567af88493bfa38d4175666
varinimittal <47278598+varinimittal@users.noreply.github.com>
2020-01-19 20:16:21 +0530

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -1,237 +1,166 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-#include <omp.h>
-#include "kmeans.h"
-
-
-/*----< euclid_dist_2() >----------------------------------------------------*/
-/* square of Euclid distance between two multi-dimensional points            */
-__inline static
-float euclid_dist_2(int    numdims,  /* no. dimensions */
-                    float *coord1,   /* [numdims] */
-                    float *coord2)   /* [numdims] */
+int main()
 {
-    int i;
-    float ans=0.0;
+    int n,m,t,pos;//where m= number of ants and n = no of cities
+    printf("enter the no of cities= ");
+    scanf("%d",&n);
+    m=n;
+    t=1;//intial pheromone
+    float a,b;
+    a=0.7,b=0.7,pos=0;
+    float probability[n];
+    float dist_matrix[n][n];
+    float pheromone_matrix[n][n];
+    float visible_matrix[n][n];
+    float visited_edge_matrix[n][n];
+   /*Counter variables for the loop*/
+   int i, j,k;
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%f", &dist_matrix[i][j]);
+      }
+   }
+   //Displaying array elements
+   printf("Distance Matrix:\n");
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+         printf("%f ", dist_matrix[i][j]);
+         if(j==(n-1)){
+            printf("\n");
+         }
+      }
+   }
 
-    for (i=0; i<numdims; i++)
-        ans += (coord1[i]-coord2[i]) * (coord1[i]-coord2[i]);
+   //Visible matrix
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+          if(i==j)
+          {
+              visible_matrix[i][j]=0;
+          }
+          else
+         visible_matrix[i][j]=1/dist_matrix[i][j];
+      }
+   }
+   printf("Visibility of edge Matrix:\n");
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+         printf("%f ", visible_matrix[i][j]);
+         if(j==(n-1)){
+            printf("\n");
+         }
+      }
+   }
+   //Pheromone matrix
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+          if(i==j)
+          {
+              pheromone_matrix[i][j]=0;
+          }
+          else
+         pheromone_matrix[i][j]=t;
+      }
+   }
+   printf("Pheromone Matrix:\n");
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+         printf("%f ", pheromone_matrix[i][j]);
+         if(j==(n-1)){
+            printf("\n");
+         }
+      }
+   }
+   //visited edge matrix
+   for(i=0; i<n; i++) {
+      for(j=0;j<n;j++) {
+          if(i==j)
+          {
+              visited_edge_matrix[i][j]=0;
+          }
+          else
+         visited_edge_matrix[i][j]=1;
+      }
+   }
+       //for calculating probability
+       int position[n];
+       int path[n];
+   int c,s,co,location=1,inc=0,f=0;
+   float sum,maximum;
+   c=0;
+   s=0;
+   for(int g=0;g<n;g++)
+                 probability[g]=0;
+       while(f<n)
+       {
+           //change 0 to starting point
+           sum=0;
+           for(k=0;k<m;k++)
+           {
+               if(s==k)
+               {
+                   continue;
+               }
+               else
+               {
+                   if(visited_edge_matrix[s][k]!=0 && k!=0) //afterwards change zero to starting point ehich is the number of ant
+                   sum+=pow(pheromone_matrix[s][k],a)*pow(visible_matrix[s][k],b);
+               }
 
-    return(ans);
-}
-
-/*----< find_nearest_cluster() >---------------------------------------------*/
-__inline static
-int find_nearest_cluster(int     numClusters, /* no. clusters */
-                         int     numCoords,   /* no. coordinates */
-                         float  *object,      /* [numCoords] */
-                         float **clusters)    /* [numClusters][numCoords] */
-{
-    int   index, i;
-    float dist, min_dist;
-
-    /* find the cluster id that has min distance to object */
-    index    = 0;
-    min_dist = euclid_dist_2(numCoords, object, clusters[0]);
-
-    for (i=1; i<numClusters; i++) {
-        dist = euclid_dist_2(numCoords, object, clusters[i]);
-        /* no need square root */
-        if (dist < min_dist) { /* find the min and its array index */
-            min_dist = dist;
-            index    = i;
-        }
-    }
-    return(index);
-}
-
-
-/*----< kmeans_clustering() >------------------------------------------------*/
-/* return an array of cluster centers of size [numClusters][numCoords]       */
-float** omp_kmeans(int     is_perform_atomic, /* in: */
-                   float **objects,           /* in: [numObjs][numCoords] */
-                   int     numCoords,         /* no. coordinates */
-                   int     numObjs,           /* no. objects */
-                   int     numClusters,       /* no. clusters */
-                   float   threshold,         /* % objects change membership */
-                   int    *membership)        /* out: [numObjs] */
-{
-
-    int      i, j, k, index, loop=0;
-    int     *newClusterSize; /* [numClusters]: no. objects assigned in each
-                                new cluster */
-    float    delta;          /* % of objects change their clusters */
-    float  **clusters;       /* out: [numClusters][numCoords] */
-    float  **newClusters;    /* [numClusters][numCoords] */
-    double   timing;
-
-    int      nthreads;             /* no. threads */
-    int    **local_newClusterSize; /* [nthreads][numClusters] */
-    float ***local_newClusters;    /* [nthreads][numClusters][numCoords] */
-
-    nthreads = omp_get_max_threads();
-
-    /* allocate a 2D space for returning variable clusters[] (coordinates
-       of cluster centers) */
-    clusters    = (float**) malloc(numClusters *             sizeof(float*));
-    assert(clusters != NULL);
-    clusters[0] = (float*)  malloc(numClusters * numCoords * sizeof(float));
-    assert(clusters[0] != NULL);
-    for (i=1; i<numClusters; i++)
-        clusters[i] = clusters[i-1] + numCoords;
-
-    /* pick first numClusters elements of objects[] as initial cluster centers*/
-    for (i=0; i<numClusters; i++)
-        for (j=0; j<numCoords; j++)
-            clusters[i][j] = objects[i][j];
-
-    /* initialize membership[] */
-    for (i=0; i<numObjs; i++) membership[i] = -1;
-
-    /* need to initialize newClusterSize and newClusters[0] to all 0 */
-    newClusterSize = (int*) calloc(numClusters, sizeof(int));
-    assert(newClusterSize != NULL);
-
-    newClusters    = (float**) malloc(numClusters *            sizeof(float*));
-    assert(newClusters != NULL);
-    newClusters[0] = (float*)  calloc(numClusters * numCoords, sizeof(float));
-    assert(newClusters[0] != NULL);
-    for (i=1; i<numClusters; i++)
-        newClusters[i] = newClusters[i-1] + numCoords;
-
-    if (!is_perform_atomic) {
-        /* each thread calculates new centers using a private space,
-           then thread 0 does an array reduction on them. This approach
-           should be faster */
-        local_newClusterSize    = (int**) malloc(nthreads * sizeof(int*));
-        assert(local_newClusterSize != NULL);
-        local_newClusterSize[0] = (int*)  calloc(nthreads*numClusters,
-                                                 sizeof(int));
-        assert(local_newClusterSize[0] != NULL);
-        for (i=1; i<nthreads; i++)
-            local_newClusterSize[i] = local_newClusterSize[i-1]+numClusters;
-
-        /* local_newClusters is a 3D array */
-        local_newClusters    =(float***)malloc(nthreads * sizeof(float**));
-        assert(local_newClusters != NULL);
-        local_newClusters[0] =(float**) malloc(nthreads * numClusters *
-                                               sizeof(float*));
-        assert(local_newClusters[0] != NULL);
-        for (i=1; i<nthreads; i++)
-            local_newClusters[i] = local_newClusters[i-1] + numClusters;
-        for (i=0; i<nthreads; i++) {
-            for (j=0; j<numClusters; j++) {
-                local_newClusters[i][j] = (float*)calloc(numCoords,
-                                                         sizeof(float));
-                assert(local_newClusters[i][j] != NULL);
-            }
-        }
-    }
-
-    if (_debug) timing = omp_get_wtime();
-    do {
-        delta = 0.0;
-
-        if (is_perform_atomic) {
-            #pragma omp parallel for \
-                    private(i,j,index) \
-                    firstprivate(numObjs,numClusters,numCoords) \
-                    shared(objects,clusters,membership,newClusters,newClusterSize) \
-                    schedule(static) \
-                    reduction(+:delta)
-            for (i=0; i<numObjs; i++) {
-                /* find the array index of nestest cluster center */
-                index = find_nearest_cluster(numClusters, numCoords, objects[i],
-                                             clusters);
-
-                /* if membership changes, increase delta by 1 */
-                if (membership[i] != index) delta += 1.0;
-
-                /* assign the membership to object i */
-                membership[i] = index;
-
-                /* update new cluster centers : sum of objects located within */
-                #pragma omp atomic
-                newClusterSize[index]++;
-                for (j=0; j<numCoords; j++)
-                    #pragma omp atomic
-                    newClusters[index][j] += objects[i][j];
-            }
-        }
-        else {
-            #pragma omp parallel \
-                    shared(objects,clusters,membership,local_newClusters,local_newClusterSize)
-            {
-                int tid = omp_get_thread_num();
-                #pragma omp for \
-                            private(i,j,index) \
-                            firstprivate(numObjs,numClusters,numCoords) \
-                            schedule(static) \
-                            reduction(+:delta)
-                for (i=0; i<numObjs; i++) {
-                    /* find the array index of nestest cluster center */
-                    index = find_nearest_cluster(numClusters, numCoords,
-                                                 objects[i], clusters);
-
-                    /* if membership changes, increase delta by 1 */
-                    if (membership[i] != index) delta += 1.0;
-
-                    /* assign the membership to object i */
-                    membership[i] = index;
-
-                    /* update new cluster centers : sum of all objects located
-                       within (average will be performed later) */
-                    local_newClusterSize[tid][index]++;
-                    for (j=0; j<numCoords; j++)
-                        local_newClusters[tid][index][j] += objects[i][j];
+           }
+            for(i=0;i<m;i++)
+              {
+                  if(s==i)
+                  continue;
+                  else
+                  {
+                      if(visited_edge_matrix[s][i]!=0 && i!=0)
+                      //afterwards change zero to starting point ehich is the number of ant
+                      {
+                      probability[c]=pow(pheromone_matrix[s][i],a)*pow(visible_matrix[s][i],b)/sum;
+                      position[c]=i;
+                      printf("%d ",position[c]);
+                      }
+                  }
+                  printf("\n %f probability",probability[c]);
+                  c++;
+              }
+              //for calculating maximum pheromone concentration
+             maximum = probability[0];
+              for (co = 1; co <n; co++)
+               {
+                  if (probability[co] > maximum)
+                    {
+                     maximum = probability[co];
+                      location = co;
+                     }
                 }
-            } /* end of #pragma omp parallel */
 
-            /* let the main thread perform the array reduction */
-            for (i=0; i<numClusters; i++) {
-                for (j=0; j<nthreads; j++) {
-                    newClusterSize[i] += local_newClusterSize[j][i];
-                    local_newClusterSize[j][i] = 0.0;
-                    for (k=0; k<numCoords; k++) {
-                        newClusters[i][k] += local_newClusters[j][i][k];
-                        local_newClusters[j][i][k] = 0.0;
+                printf("\nlocation %d",location);
+                for(int g=0;g<n;g++)
+                {
+                    if(g==location)
+                    {
+                     int l=position[location];
+                        visited_edge_matrix[s][l]=0;
+                        printf("%d",l);
+                        s=position[location];
+                        path[inc]=position[location];
                     }
                 }
-            }
-        }
+                 for(int g=0;g<n;g++)
+                 probability[g]=0;
+                f++;
 
-        /* average the sum and replace old cluster centers with newClusters */
-        for (i=0; i<numClusters; i++) {
-            for (j=0; j<numCoords; j++) {
-                if (newClusterSize[i] > 1)
-                    clusters[i][j] = newClusters[i][j] / newClusterSize[i];
-                newClusters[i][j] = 0.0;   /* set back to 0 */
-            }
-            newClusterSize[i] = 0;   /* set back to 0 */
-        }
-            
-        delta /= numObjs;
-    } while (delta > threshold && loop++ < 500);
 
-    if (_debug) {
-        timing = omp_get_wtime() - timing;
-        printf("nloops = %2d (T = %7.4f)",loop,timing);
-    }
+       }
 
-    if (!is_perform_atomic) {
-        free(local_newClusterSize[0]);
-        free(local_newClusterSize);
 
-        for (i=0; i<nthreads; i++)
-            for (j=0; j<numClusters; j++)
-                free(local_newClusters[i][j]);
-        free(local_newClusters[0]);
-        free(local_newClusters);
-    }
-    free(newClusters[0]);
-    free(newClusters);
-    free(newClusterSize);
-
-    return clusters;
+   return 0;
 }
+
+
+
